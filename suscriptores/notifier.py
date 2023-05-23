67c45699674e369e7b29d2c3cd17b0204f63b8fa
J Luis <vargasibarra01@gmail.com>
2023-05-23 13:39:39 -0600

    Mostrar mensaje del callback

--- suscriptores/notifier.py
+++ suscriptores/notifier.py
@@ -1,72 +1,13 @@
-##!/usr/bin/env python
-# -*- coding: utf-8 -*-
-#-------------------------------------------------------------------------
-# Archivo: notifier.py
-# Capitulo: Estilo Publica-Suscribe
-# Autor(es): Perla Velasco & Yonathan Mtz. & Jorge Solís
-# Version: 3.0.0 Marzo 2022
-# Descripción:
-#
-#   Esta clase define el suscriptor que recibirá mensajes desde el distribuidor de mensajes
-#   y lo notificará a un(a) enfermero(a) én particular para la atención del adulto mayor en
-#   cuestión
-#
-#   Este archivo también define el punto de ejecución del Suscriptor
-#
-#   A continuación se describen los métodos que se implementaron en esta clase:
-#
-#                                             Métodos:
-#           +------------------------+--------------------------+-----------------------+
-#           |         Nombre         |        Parámetros        |        Función        |
-#           +------------------------+--------------------------+-----------------------+
-#           |       __init__()       |  - self: definición de   |  - constructor de la  |
-#           |                        |    la instancia de la    |    clase              |
-#           |                        |    clase                 |                       |
-#           +------------------------+--------------------------+-----------------------+
-#           |       suscribe()       |  - self: definición de   |  - inicializa el      |
-#           |                        |    la instancia de la    |    proceso de         |
-#           |                        |    clase                 |    monitoreo de       |
-#           |                        |                          |    signos vitales     |
-#           +------------------------+--------------------------+-----------------------+
-#           |        consume()       |  - self: definición de   |  - realiza la         |
-#           |                        |    la instancia de la    |    suscripción en el  |
-#           |                        |    clase                 |    distribuidor de    |
-#           |                        |  - queue: ruta a la que  |    mensajes para      |
-#           |                        |    el suscriptor está    |    comenzar a recibir |
-#           |                        |    interesado en recibir |    mensajes           |
-#           |                        |    mensajes              |                       |
-#           |                        |  - callback: accion a    |                       |
-#           |                        |    ejecutar al recibir   |                       |
-#           |                        |    el mensaje desde el   |                       |
-#           |                        |    distribuidor de       |                       |
-#           |                        |    mensajes              |                       |
-#           +------------------------+--------------------------+-----------------------+
-#           |       callback()       |  - self: definición de   |  - envía a través de  |
-#           |                        |    la instancia de la    |    telegram los datos |
-#           |                        |    clase                 |    del adulto mayor   |
-#           |                        |  - ch: canal de          |    recibidos desde el |
-#           |                        |    comunicación entre el |    distribuidor de    |
-#           |                        |    suscriptor y el       |    mensajes           |
-#           |                        |    distribuidor de       |                       |
-#           |                        |    mensajes [propio de   |                       |
-#           |                        |    RabbitMQ]             |                       |
-#           |                        |  - method: método de     |                       |
-#           |                        |    conexión utilizado en |                       |
-#           |                        |    la suscripción        |                       |
-#           |                        |    [propio de RabbitMQ]  |                       |
-#           |                        |  - properties:           |                       |
-#           |                        |    propiedades de la     |                       |
-#           |                        |    conexión [propio de   |                       |
-#           |                        |    RabbitMQ]             |                       |
-#           |                        |  - body: contenido del   |                       |
-#           |                        |    mensaje recibido      |                       |
-#           +------------------------+--------------------------+-----------------------+
-#
-#-------------------------------------------------------------------------
 import json, time, stomp, sys
 
-class Notifier:
+class Message(stomp.ConnectionListener):
+    def on_message(self, message):
+        data = json.loads(message.body)
+        print(f"ADVERTENCIA!!!\n[{data['wearable']['date']}]: asistir al paciente {data['name']} {data['last_name']}...\nssn: {data['ssn']}, edad: {data['age']}, temperatura: {round(data['wearable']['temperature'], 1)}, ritmo cardiaco: {data['wearable']['heart_rate']}, presión arterial: {data['wearable']['blood_pressure']}, dispositivo: {data['wearable']['id']}")
 
+
+class Notifier:
+    
     def __init__(self):
         self.topic = "notifier"
         self.token = ""
@@ -75,14 +16,14 @@ class Notifier:
     def suscribe(self):
         print("Inicio de gestión de notificaciones...")
         print()
-        self.consume(queue=self.topic, callback=self.callback)
+        self.consume(queue=self.topic)
 
-    def consume(self, queue, callback):
+    def consume(self, queue):
         try:
-            conn = stomp.Connection(host_and_ports=[('localhost', 15672)])
-            conn.set_listener('', callback)
-            conn.connect()
-            conn.subscribe(destination=queue, id=1, ack='auto')
+            conn = stomp.Connection(host_and_ports=[('localhost', 61613)])
+            conn.set_listener('callback', Message())
+            conn.connect(wait=True)
+            conn.subscribe(destination=queue, headers='', id=1)
             while True:
                 time.sleep(1)
         
